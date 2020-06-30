b3125dc43a7deb4d84960cca25b6b9a3cb628855
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-30 18:11:10 +0900

    created main component as component element

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../component/searchbar'
-import yelp from '../api/yelp'
 import useResults from '../hooks/useResults'
+import ResultsList from '../component/ResultsList'
 
 const HomePage = () => {
   const [term, setTerm] = useState('');
@@ -17,6 +17,19 @@ const HomePage = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList 
+        title="Cost Effective"
+
+      />
+      <ResultsList 
+        title="Bit Pricier"
+
+      />
+      <ResultsList 
+        title="Big Spender"
+
+      />
+
     </View>
   )
 };
