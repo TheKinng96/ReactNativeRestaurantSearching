fa8e6f9eaed53226e21cabfd333c1c8dbef96119
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-07-04 22:38:07 +0900

    finished the api rendering of the detail page

--- src/pages/detailpage.jsx
+++ src/pages/detailpage.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
+import yelp from '../api/yelp'
 
 const DetailPage = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam('id')
 
-  console.log(id)
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
   return (
     <View> 
-      <Text>This is DetailPage</Text>
+      <Text>{result.name}</Text>
+      <FlatList 
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({item}) => {
+          return <Image style={style.image} source={{ uri: item }} />
+        }}
+      />
     </View>
   )
 }
 
-const style = StyleSheet.create({})
+const style = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300
+  }
+})
 
 export default DetailPage;
